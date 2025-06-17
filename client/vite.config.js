1af97d47599788edf1a1aea4230acb43e94cadd9
ClaeysMaxim <maxim.claeys@student.arteveldehs.be>
2025-06-17 21:06:50 +0200

    fix: update vite.config.js to use dynamic configuration based on environment mode

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
   ],
   publicDir: "../public",
-});
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  define: {
+    "import.meta.env.PROD": process.env.NODE_ENV === "production",
+  },
+}));
